9b955fdadf7646520dcbb6939c8b6e013424b245
Ash Walker <ashurstwalker@gmail.com>
2022-08-17 13:55:58 -0400

    chore: format

--- flake.nix
+++ flake.nix
@@ -26,13 +26,22 @@
     with builtins; let
       std = nixpkgs.lib;
       hlib = homelib.lib;
-      nixpkgsFor = hlib.genNixpkgsFor { inherit nixpkgs; overlays = []; };
+      nixpkgsFor = hlib.genNixpkgsFor {
+        inherit nixpkgs;
+        overlays = [];
+      };
     in {
       formatter = std.mapAttrs (system: pkgs: pkgs.default) inputs.alejandra.packages;
       homeManagerModules.default = let
         stateVersion = "22.11";
       in
-        { lib, options, config, specialArgs, modulesPath }: {
+        {
+          lib,
+          options,
+          config,
+          specialArgs,
+          modulesPath,
+        }: {
           options = with lib; {
             # signal.base.enable = (mkEnableOption "base configuration") // {default = true;};
             system.isNixOS = (mkEnableOption "allows configuration specific to NixOS systems") // {default = true;};
@@ -45,11 +54,13 @@
             home.stateVersion = stateVersion;
           };
         };
-      homeConfigurations = mapAttrs (system: pkgs: {
-        default = hlib.genHomeConfiguration {
-          inherit pkgs inputs;
-        };
-      }) nixpkgsFor;
+      homeConfigurations =
+        mapAttrs (system: pkgs: {
+          default = hlib.genHomeConfiguration {
+            inherit pkgs inputs;
+          };
+        })
+        nixpkgsFor;
       packages = hlib.genHomeActivationPackages self.homeConfigurations;
       apps = hlib.genHomeActivationApps self.homeConfigurations;
     };
