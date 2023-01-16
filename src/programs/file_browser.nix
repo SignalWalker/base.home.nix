330078de891a38b8a3ce6912304b937412516e81
Ash Walker <ashurstwalker@gmail.com>
2023-01-15 21:46:38 -0500

    feat: ranger file browser

---
+++ src/programs/file_browser.nix
@@ -0,0 +1,24 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+with builtins; let
+  std = pkgs.lib;
+in {
+  options = with lib; {};
+  disabledModules = [];
+  imports = lib.signal.fs.path.listFilePaths ./file_browser;
+  config = {
+    programs.ranger = {
+      enable = true;
+      settings = {
+        draw_borders = "separators";
+        unicode_ellipsis = true;
+        update_title = true;
+      };
+    };
+  };
+  meta = {};
+}
---
+++ src/programs/file_browser/ranger.nix
@@ -0,0 +1,58 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+with builtins; let
+  std = pkgs.lib;
+  ranger = config.programs.ranger;
+in {
+  options = with lib; {
+    programs.ranger = {
+      enable = mkEnableOption "ranger file browser";
+      package = mkPackageOption pkgs "ranger" {};
+      settings = mkOption {
+        type = types.submoduleWith {
+          modules = [
+            ({
+              config,
+              lib,
+              ...
+            }: {
+              freeformType = with lib; types.attrsOf types.anything;
+              options = with lib; {};
+            })
+          ];
+        };
+      };
+      extraConfig = mkOption {
+        type = types.lines;
+        default = "";
+      };
+    };
+  };
+  disabledModules = [];
+  imports = [];
+  config = lib.mkIf ranger.enable {
+    home.packages = [ranger.package];
+    xdg.configFile."ranger/rc.conf" = {
+      text = let
+        valToStr = val:
+          if isBool val
+          then
+            (
+              if val
+              then "true"
+              else "false"
+            )
+          else (toString val);
+      in
+        std.concatStringsSep "\n" (
+          (map (key: "set ${key} ${valToStr ranger.settings.${key}}") (attrNames ranger.settings))
+          ++ [ranger.extraConfig]
+        );
+    };
+  };
+  meta = {};
+}
