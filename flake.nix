e00821d31f8404782ea52d474faacea833c70965
Ash Walker <ash@ashwalker.net>
2023-07-15 14:27:50 -0400

    ...

--- flake.nix
+++ flake.nix
@@ -1,17 +1,17 @@
 {
   description = "Home manager configuration - base";
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     alejandra = {
-      url = github:kamadorueda/alejandra;
+      url = "github:kamadorueda/alejandra";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = github:nix-community/home-manager;
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     homelib = {
-      url = github:signalwalker/nix.home.lib;
+      url = "github:signalwalker/nix.home.lib";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.alejandra.follows = "alejandra";
       inputs.home-manager.follows = "home-manager";
@@ -25,42 +25,22 @@
   outputs = inputs @ {
     self,
     nixpkgs,
-    home-manager,
-    homelib,
     ...
   }:
     with builtins; let
       std = nixpkgs.lib;
-      hlib = homelib.lib;
-      home = hlib.home;
-      signal = hlib.signal;
     in {
       formatter = std.mapAttrs (system: pkgs: pkgs.default) inputs.alejandra.packages;
-      signalModules.default = {
-        name = "home.base.default";
-        dependencies = signal.flake.set.toDependencies {
-          flakes = inputs;
-          filter = ["alejandra"];
-        };
-        outputs = dependencies: {
-          homeManagerModules = {...}: {
-            imports = [./home-manager.nix];
-            config = {
-              home.stateVersion = "22.11";
-              lib.signal = dependencies.homelib.lib;
-              signal.base.homeManagerSrc = dependencies.home-manager;
-              programs.fish.pluginSources = with dependencies; {
-                fzf = fishFzf;
-              };
-            };
+      homeManagerModules.default = {...}: {
+        imports = [./home-manager.nix];
+        config = {
+          home.stateVersion = "22.11";
+          lib.signal = inputs.homelib.lib;
+          signal.base.homeManagerSrc = inputs.home-manager;
+          programs.fish.pluginSources = {
+            fzf = inputs.fishFzf;
           };
         };
       };
-      homeConfigurations = home.configuration.fromFlake {
-        flake = self;
-        flakeName = "home.base";
-      };
-      packages = home.package.fromHomeConfigurations self.homeConfigurations;
-      apps = home.app.fromHomeConfigurations self.homeConfigurations;
     };
 }
