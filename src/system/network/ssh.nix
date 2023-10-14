067e1a397bbd6b2f15fcc12bbebc09abea08099d
Ash Walker <ashurstwalker@gmail.com>
2023-10-14 16:04:49 -0400

    ...

--- src/system/network/ssh.nix
+++ src/system/network/ssh.nix
@@ -20,7 +20,7 @@ in {
       enable = true;
       controlMaster = "auto";
       controlPath = "$\{XDG_RUNTIME_DIR\}/ssh/socket-%r@%h:%p";
-      controlPersist = "6m";
+      controlPersist = "no";
       extraOptionOverrides = {
         Ciphers = "aes128-gcm@openssh.com,aes256-gcm@openssh.com,chacha20-poly1305@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr";
       };
